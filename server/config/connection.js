44ddc160d65696a177f6a6eff993e21b687f5857
Dillon Cleaver <cleadi@fastmail.com>
2021-11-24 16:30:34 -0600

    Still trying to fix Heroku build #3.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/searchbooks", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
